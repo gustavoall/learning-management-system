c32a3cf8fdc8f60a71318bf4f1efc37669e425a1
gustavoall <gustavo@estilodev.com.br>
2025-07-06 23:00:55 -0300

    update variables

--- src/app/views/dashboard/dashboard.vue
+++ src/app/views/dashboard/dashboard.vue
@@ -1,8 +1,15 @@
 <template>
     <div id="dashboard">
-        <h1>Dashboard</h1>
-        <p>Bem-vindo ao seu LMS!</p>
-        <a href="/lesson/1">Ir para Lição 1</a>
+        <section class="section-01">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-01">
+                        <h1>Bem-vindo ao seu LMS!</h1>
+                        <!-- <a href="/lesson/1">Ir para Lição 1</a> -->
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
